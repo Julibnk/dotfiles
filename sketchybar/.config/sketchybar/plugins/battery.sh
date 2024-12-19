9b932d410fe54632e688336e7acfa746544b3f2c
julibnk <fernandezfernandezjulian@gmail.com>
2024-12-19 21:51:44 +0100

    colors

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -10,47 +10,47 @@ if [[ $IS_CHARGING == "" ]]; then
 	case ${BATT_PERCENT} in
 	100)
 		ICON=${ICONS_BATTERY[10]}
-		COLOR=$COLOR_GREEN_BRIGHT
+		COLOR=$GREEN
 		;;
 	9[0-9])
 		ICON=${ICONS_BATTERY[9]}
-		COLOR=$COLOR_GREEN_BRIGHT
+		COLOR=$GREEN
 		;;
 	8[0-9])
 		ICON=${ICONS_BATTERY[8]}
-		COLOR=$COLOR_GREEN
+		COLOR=$GREEN
 		;;
 	7[0-9])
 		ICON=${ICONS_BATTERY[7]}
-		COLOR=$COLOR_YELLOW_BRIGHT
+		COLOR=$GREEN
 		;;
 	6[0-9])
 		ICON=${ICONS_BATTERY[6]}
-		COLOR=$COLOR_YELLOW_BRIGHT
+		COLOR=$YELLOW
 		;;
 	5[0-9])
 		ICON=${ICONS_BATTERY[5]}
-		COLOR=$COLOR_YELLOW
+		COLOR=$YELLOW
 		;;
 	4[0-9])
 		ICON=${ICONS_BATTERY[4]}
-		COLOR=$COLOR_YELLOW
+		COLOR=$ORANGE
 		;;
 	3[0-9])
 		ICON=${ICONS_BATTERY[3]}
-		COLOR=$COLOR_YELLOW
+		COLOR=$ORANGE
 		;;
 	2[0-9])
 		ICON=${ICONS_BATTERY[2]}
-		COLOR=$COLOR_RED_BRIGHT
+		COLOR=$RED
 		;;
 	1[0-9])
 		ICON=${ICONS_BATTERY[1]}
-		COLOR=$COLOR_RED
+		COLOR=$RED
 		;;
 	*)
 		ICON=${ICONS_BATTERY[0]}
-		COLOR=$COLOR_RED
+		COLOR=$RED
 		;;
 	esac
 else
@@ -89,7 +89,7 @@ else
 		ICON=${ICONS_BATTERY_CHARGING[0]}
 		;;
 	esac
-	COLOR=$COLOR_GREEN_BRIGHT
+	COLOR=$GREEN
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
@@ -98,44 +98,4 @@ sketchybar --set battery icon=$ICON \
 	icon.color=$COLOR \
 	label="${BATT_PERCENT}%" \
 	label.color=$COLOR
-# # Battery is here bcause the ICON_COLOR doesn't play well with all background colors
-#
-# PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
-# CHARGING=$(pmset -g batt | grep 'AC Power')
-#
-# if [ $PERCENTAGE = "" ]; then
-#     exit 0
-# fi
-#
-# case ${PERCENTAGE} in
-# [8-9][0-9] | 100)
-#     ICON=""
-#     ICON_COLOR=0xffa6da95
-#     ;;
-# 7[0-9])
-#     ICON=""
-#     ICON_COLOR=0xffeed49f
-#     ;;
-# [4-6][0-9])
-#     ICON=""
-#     ICON_COLOR=0xfff5a97f
-#     ;;
-# [1-3][0-9])
-#     ICON=""
-#     ICON_COLOR=0xffee99a0
-#     ;;
-# [0-9])
-#     ICON=""
-#     ICON_COLOR=0xffed8796
-#     ;;
-# esac
-#
-# if [[ $CHARGING != "" ]]; then
-#     ICON=""
-#     ICON_COLOR=0xffeed49f
-# fi
-#
-# sketchybar --set $NAME \
-#     icon=$ICON \
-#     label="${PERCENTAGE}%" \
-#     icon.color=${ICON_COLOR}
+
