32f66b5fa5c6d37064f28cfedf5bc6413b3d069c
julibnk <fernandezfernandezjulian@gmail.com>
2025-04-08 09:37:53 +0200

    nvim config

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,50 +1,14 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      CustomOilBar = function()
-        local path = vim.fn.expand '%'
-        path = path:gsub('oil://', '')
-
-        return '  ' .. vim.fn.fnamemodify(path, ':.')
-      end
-      local oil = require 'oil'
-
-      oil.setup {
-        columns = { 'icon' },
-        keymaps = {
-          ['l'] = { 'actions.select', mode = 'n' },
-          ['h'] = { 'actions.parent', mode = 'n' },
-          ['-'] = 'actions.close',
-          ['<C-h>'] = false,
-          ['<C-l>'] = false,
-          ['<C-k>'] = false,
-          ['<C-j>'] = false,
-          ['<M-h>'] = 'actions.select_split',
-        },
-        win_options = {
-          winbar = '%{v:lua.CustomOilBar()}',
-        },
-        view_options = {
-          show_hidden = true,
-        },
-      }
-      -- Open parent directory in current window
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-      -- Open oil in preview mode
-      -- vim.keymap.set('n', '-', function()
-      --   oil.open()
-      --   -- Wait until oil has opened, for a maximum of 1 second.
-      --   vim.wait(1000, function()
-      --     return oil.get_cursor_entry() ~= nil
-      --   end)
-      --   if oil.get_cursor_entry() then
-      --     oil.open_preview()
-      --   end
-      -- end)
-      -- Open parent directory in floating window
-      -- vim.keymap.set('n', '<space>-', require('oil').toggle_float)
-    end,
-  },
+	'stevearc/oil.nvim',
+	keys = {
+		{ '-' , '<cmd>Oil<cr>', desc = 'Open Oil' }, 
+	},
+	opts = {
+		keymaps = {
+			["h"] = {"actions.parent" },
+			["-"] = {"actions.close" },
+			["l"] = {"actions.select" },
+		}
+	
+}
 }
