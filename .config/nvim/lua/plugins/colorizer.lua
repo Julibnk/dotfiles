09ed2aea9fd2064f584b0ac0ee185192a14ad1ba
julibnk <fernandezfernandezjulian@gmail.com>
2025-07-14 00:06:20 +0200

    nvim colorizer

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,14 +1,21 @@
---FIX: RRGGBBA with sketchybar status colors doesnt work
 return {
-	"norcalli/nvim-colorizer.lua",
-	enabled = false,
-	---@module 'colorizer'
+	"NvChad/nvim-colorizer.lua",
 	opts = {
-		filetypes = {
-			"*",
-		},
+		-- filetypes = { "*" },
 		user_default_options = {
-			RRGGBBAA = true,
+			RGB = true, -- #RGB
+			RRGGBB = true, -- #RRGGBB
+			names = false, -- "blue"
+			RRGGBBAA = true, -- #RRGGBBAA
+			AARRGGBB = true, -- 0xAARRGGBB ← important
+			mode = "background", -- or "foreground"
+			css = true,
+			css_fn = true,
+			tailwind = true,
+			sass = { enable = true, parsers = { "css" } },
 		},
 	},
+	config = function(_, opts)
+		require("colorizer").setup(opts)
+	end,
 }
