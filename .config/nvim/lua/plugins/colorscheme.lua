ff385eea404b552647cca8104965f275d1d0386c
julibnk <fernandezfernandezjulian@gmail.com>
2026-03-18 13:29:36 +0100

    fix(nvim): todo comments highlight groups  not loading with kanso theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -17,6 +17,14 @@ return {
 				minimal = true,
 			})
 			vim.cmd("colorscheme kanso-zen")
+
+			--FIX:
+			-- If i delete this call the todo-comments highlight groups doesnt work
+			-- But it is only with kanso themes, with kanagawa the hg works well
+			-- Also if i execute := vim.cmd("colorscheme kanso-zen") manually, gets fixed
+			-- I think this info might be usefull https://lazy.folke.io/spec/lazy_loading#-colorschemes
+			-- I think the main issue is whith kanso theme, not todo-comments
+			vim.defer_fn(require("todo-comments.config").colors, 10)
 		end,
 	},
 	{
